4f5885448171b7ae46f93b0c1cf9fb66878a8b94
강민석 <bluekms@shiftup.co.kr>
2021-07-23 02:04:28 +0900

    선택정렬

---
+++ Algorithm/selection_sort.py
@@ -0,0 +1,8 @@
+from typing import List
+
+input = ['개구리', '거위', '고릴라', '기린', '닭', '말', '북극곰', '얼룩말', '코끼리']
+
+def SelectionSort(arr: List[str]) -> List[str]:
+    return arr
+
+print(SelectionSort(input))
