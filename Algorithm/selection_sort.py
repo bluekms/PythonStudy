4e734ce0404c272e7939ab2adbc41b7184c283ce
minseok.kang <bluekms21@naver.com>
2021-07-24 12:54:29 +0900

    선택정렬

--- Algorithm/selection_sort.py
+++ Algorithm/selection_sort.py
@@ -1,8 +1,21 @@
 from typing import List
 
-input = ['개구리', '거위', '고릴라', '기린', '닭', '말', '북극곰', '얼룩말', '코끼리']
+
+def GetMinIndex(arr: List[str]) -> int:
+    minIndex = 0
+    for i in range(len(arr)):
+        if arr[i] < arr[minIndex]:
+            minIndex = i
+    return minIndex
+
 
 def SelectionSort(arr: List[str]) -> List[str]:
-    return arr
+    result = []
+    while input:
+        minIndex = GetMinIndex(arr)
+        result.append(arr.pop(minIndex))
+    return result
 
+
+input = ["개구리", "거위", "고릴라", "기린", "닭", "말", "북극곰", "얼룩말", "코끼리"]
 print(SelectionSort(input))
